a758c9f1e8380680959a6852483a4c7ae7c19baa
shailus1029 <shailus1029@gmail.com>
2019-08-14 08:46:06 +0530

    logger implementation is done

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const router = express.Router();
-const db = require('./config/db');
+const express = require("express");
+const bodyParser = require("body-parser");
+const db = require("./config/db");
 
 // using http server to bind the socket io
-const http = require('http');
+const http = require("http");
 
 //call the database connectivity function
 db();
 
 //importing routes
-const user = require('./routes/user.route');
+const user = require("./routes/user.route");
 
 //initilizing our express app
 const app = express();
 
 //adding body parser
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/api/user' ,user);
+app.use("/api/user", user);
 
 //defining the port to run the server
 const port = 8000;
@@ -28,5 +27,5 @@ var server = http.createServer(app);
 
 //listening to the port 8000
 server.listen(port, () => {
-    console.log("server is running on  port : ", port)
+  console.log("server is running on  port : ", port);
 });
